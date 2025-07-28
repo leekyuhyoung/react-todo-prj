46d2101a1580974e8e62fa933b2b4672202164fa
Lk <replaykh@gmail.com>
2025-07-28 21:51:43 +0900

    250728 weather app

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+    // setLocation({ latitude, longitude });
+    setCity(location[0].city); // 예시로 직접 지정
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text>서울</Text>
-        <Text>부산</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
+      <ScrollView
+      pagingEnabled
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={{ color: 'white' }}>날씨 정보</Text>
+        </View>
+      </ScrollView>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -15,6 +45,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'black',
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: 'white',
+    fontSize: 36,
+    fontWeight: '500',
+  },
+  weather: {
+    // 원하는 스타일 추가
+  },
+  day: {
+    width: 300,
+    alignItems: 'center',
   },
 });
